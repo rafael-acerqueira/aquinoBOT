f4cd15a45b745aaae39020c25400b9189be1f054
Rafael Aquino <programador.pleno.1@corp.agenciaacerte.com>
2019-03-28 15:11:47 -0300

    Add Logo and background

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './App.css'
 
 import Button from './components/Button'
+import { ReactComponent as Logo } from './images/logo.svg'
 
-const App = () => <Button />;
+const App = () => (
+  <div className='wrapper'>
+    <Logo />
+    <Button />
+  </div>
+);
 
 export default App;
