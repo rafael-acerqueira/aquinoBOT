602fa4f008d7f774111a6cef25ffb09928aaf71c
Rafael Aquino <programador.pleno.1@corp.agenciaacerte.com>
2019-04-10 17:50:54 -0300

    Adjust box-sizing from SearchInput

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const SearchInput = styled.input`
   background-color: #fbfbff;
   box-shadow: 0 -2px 8px 0 rgba(0, 0, 0, 0.1);
+  border: none;
+  box-sizing: border-box;
+  color: rgba(0, 0, 0, 0.3);
   padding-left: 16px;
   font-size: 20px;
   height: 50px;
